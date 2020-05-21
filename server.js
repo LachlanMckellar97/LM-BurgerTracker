e821cfd8265bd682e7c3090036b66c12a15587d9
Lachlan Mckellar <lachlan.person@gmail.com>
2020-05-21 19:06:35 +1000

    updating server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-var express = require("express")
+var express = require("express");
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+// serve static content for the app from the "public" directory
+app.use(express.static("public"));
+
+// parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// set Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// import routes and give server access to them
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+// set up application to listen on port
+app.listen(PORT, function() {
+    console.log("Server listening on http://localhost:" + PORT);
+});
