f0a789906f9aa92745aecfedfaf8b3e404efe79f
Lachlan Mckellar <lachlan.person@gmail.com>
2020-05-21 20:48:24 +1000

    controller!

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,9 +3,36 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 
-router.get("/burgers", function (req, res) {
+router.get("/", function (req, res) {
     burger.selectAll(function (data) {
-        res.render("index", { burgers: data})
+        res.render("index", {burgers: data})
     });
 });
 
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+        res.json({id: result.insertId})
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    burger.updateOne({devoured: req.body.devoured}, condition, function(result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end();
+    });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    burger.deleteOne(condition, function(result) {
+        if (result.affectedRows === 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end();
+    })
+})
+
+module.exports = router;
